9f7ee26fb5fc4a020a47216f21bc1f0b1abd7c11
Janne Vanhala <janne.vanhala@gmail.com>
2014-02-09 14:39:22 +0200

    Make serialized IDs integers
    JSON-API doesn't seem to require IDs to be strings.

--- talkoohakemisto/serializers.py
+++ talkoohakemisto/serializers.py
@@ -2,29 +2,26 @@ from marshmallow import fields, Serializer
 
 
 class MunicipalitySerializer(Serializer):
-    id = fields.String(attribute='code')
+    id = fields.Integer(attribute='code')
 
     class Meta:
         fields = ('id', 'name')
 
 
 class VoluntaryWorkTypeSerializer(Serializer):
-    id = fields.String()
-
     class Meta:
         fields = ('id', 'name')
 
 
 class VoluntaryWorkLinksSerializer(Serializer):
-    municipality = fields.String()
-    type = fields.String()
+    municipality = fields.Integer()
+    type = fields.Integer()
 
     class Meta:
         fields = ('municipality', 'type')
 
 
 class VoluntaryWorkSerializer(Serializer):
-    id = fields.String()
     links = fields.Nested(VoluntaryWorkLinksSerializer)
 
     class Meta:
--- tests/test_serializers.py
+++ tests/test_serializers.py
@@ -7,7 +7,7 @@ def test_municipality_serializer():
     municipality = factories.MunicipalityFactory.build()
     serializer = serializers.MunicipalitySerializer(municipality)
     assert serializer.data == {
-        'id': unicode(municipality.code),
+        'id': municipality.code,
         'name': municipality.name,
     }
 
@@ -16,7 +16,7 @@ def test_voluntary_work_type_serializer():
     type_ = factories.VoluntaryWorkTypeFactory.build(id=123)
     serializer = serializers.VoluntaryWorkTypeSerializer(type_)
     assert serializer.data == {
-        'id': unicode(type_.id),
+        'id': type_.id,
         'name': type_.name,
     }
 
@@ -27,14 +27,14 @@ def test_voluntary_work_serializer():
     work.municipality.code = 98
     serializer = serializers.VoluntaryWorkSerializer(work)
     assert serializer.data == {
-        'id': u'123',
+        'id': 123,
         'name': work.name,
         'organizer': u'Pekka Perusjätkä',
         'description': u'Talkootyön kuvaus',
         'street_address': u'Metsänneidonkuja 6',
         'contact_email': u'someone@example.com',
         'links': {
-            'municipality': u'98',
-            'type': u'4'
+            'municipality': 98,
+            'type': 4
         }
     }
