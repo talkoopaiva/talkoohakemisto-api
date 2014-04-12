8e75bf7c56d9163461d102790712ce1791fb4757
Jari Jaanto <jaffa@uusikanava.cat>
2014-04-12 16:13:02 +0300

    dynamically create talkoo id

--- tests/test_serializers.py
+++ tests/test_serializers.py
@@ -28,4 +28,17 @@ def test_voluntary_work_serializer():
     work.municipality.code = 98
     serializer = serializers.VoluntaryWorkSerializer(work)
 
-    assert json.dumps(serializer.data) == '{"description": "Talkooty\u00f6n kuvaus", "links": {"type": 4, "municipality": 98}, "url": "http://example.com", "goal": "Talkooty\u00f6n tavoite", "time": "11:00 - 20:00", "location": "Peruskoulu 2", "hashtag": "#hashtag", "organization": "J\u00e4rjest\u00f6", "organizer": "Pekka Perusj\u00e4tk\u00e4", "contact_phone": "+3585554444", "id": 123, "street_address": "Mets\u00e4nneidonkuja 6", "name": "Talkoot 13"}'
+    assert json.dumps(serializer.data) == '{"description": "Talkooty\u00f6n kuvaus"' \
+                                          ', "links": {"type": 4, "municipality": 98}, ' \
+                                          '"url": "http://example.com", ' \
+                                          '"goal": "Talkooty\u00f6n tavoite", ' \
+                                          '"time": "11:00 - 20:00", ' \
+                                          '"location": "Peruskoulu 2", ' \
+                                          '"hashtag": "#hashtag", ' \
+                                          '"organization": "J\u00e4rjest\u00f6", ' \
+                                          '"organizer": "Pekka Perusj\u00e4tk\u00e4", ' \
+                                          '"contact_phone": "+3585554444", ' \
+                                          '"id": 123, ' \
+                                          '"street_address": ' \
+                                          '"Mets\u00e4nneidonkuja 6", ' \
+                                          '"name": "' + work.name + '"}'
