0eaaf2ee64579200e256bd0e3d8a3bc3977c8513
Jari Jaanto <jaffa@uusikanava.cat>
2014-05-04 20:58:57 +0300

    right amount of parametrs for edit get

--- talkoohakemisto/views/voluntary_work.py
+++ talkoohakemisto/views/voluntary_work.py
@@ -45,7 +45,7 @@ def get(id):
     return jsonify(**_serialize([voluntary_work]))
 
 @voluntary_work.route('/edit/<int:id>/<string:token>')
-def editget(id):
+def editget(id, token):
     voluntary_work = VoluntaryWork.query.filter_by(id=id).one()
     return jsonify(**_serialize_edit([voluntary_work]))
 
